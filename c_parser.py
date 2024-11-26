c30c11129c2c5a113c43f97bf316612a2fcb1327
Andres Puente <00287919@uca.edu.sv>
2024-11-26 02:07:54 -0600

    arreglo funcion de parsing

--- c_parser.py
+++ c_parser.py
@@ -2,31 +2,42 @@ from utils.tokens import tokens
 from utils.tabla_parser import tabla_parser
 
 tabla = []
-pila = ['eof', 0]
+pila = ['eof', '<_BLOQUE>']
 
 def construir_arbol(lista_tokens):
-
-    for token in lista_tokens:
-        top = pila[-1]
+    print(pila)
+    iterador_token = iter(lista_tokens)
+    token = next(iterador_token)
+    print(token)
+    top = pila[-1]
+    while True:
+        print(pila)
         if top == token.type:
             if top == 'eof':
-
                 print('Terminado exitosamente')
                 return
-            
             pila.pop()
+            top = pila[-1]
+            try:
+                token = next(iterador_token)
+            except StopIteration:
+                token.type = 'eof'
             continue
         if top in tokens:               
             print("Error: se esperaba ", top)
             print('en la posicion: ', token.lexpos)
             return
+        print(top+'\t'+token.type)
         produccion = buscar_produccion(top, token.type)
+        print(produccion)
         if produccion is None:
             print("Error: NO se esperaba", token.type)
             print('en la posicion: ', token.lexpos)
             return
         pila.pop()
         agregar_produccion(pila, produccion)
+        top = pila[-1]
+       
 
 def buscar_produccion(terminal, no_terminal):
     if terminal not in tabla_parser:
@@ -36,7 +47,8 @@ def buscar_produccion(terminal, no_terminal):
     return tabla_parser[terminal][no_terminal]
 
 def agregar_produccion(pila, produccion):
-    for i in produccion:
+    for i in reversed(produccion):
         pila.append(i)
+
         
 
--- utils/tabla_parser.py
+++ utils/tabla_parser.py
@@ -1,17 +1,17 @@
 tabla_parser = {
     '<_BLOQUE>': {
-        'llave_de_inicio': ['<BLOQUE>','<_BLOQUE>'],
+        'llave_de_inicio': ['<BLOQUE>', '<_BLOQUE>'],
         'llave_de_cierre': [],
-        'identificador': ['<BLOQUE>','<_BLOQUE>'],
-        'bucle_while': ['<BLOQUE>','<_BLOQUE>'],
-        'bucle_do': ['<BLOQUE>','<_BLOQUE>'],
-        'bucle_for': ['<BLOQUE>','<_BLOQUE>'],
-        'condicion_if': ['<BLOQUE>','<_BLOQUE>'],
-        'condicion_else': ['<BLOQUE>','<_BLOQUE>'],
+        'identificador': ['<BLOQUE>', '<_BLOQUE>'],
+        'bucle_while': ['<BLOQUE>', '<_BLOQUE>'],
+        'bucle_do': ['<BLOQUE>', '<_BLOQUE>'],
+        'bucle_for': ['<BLOQUE>', '<_BLOQUE>'],
+        'condicion_if': ['<BLOQUE>', '<_BLOQUE>'],
+        'condicion_else': ['<BLOQUE>', '<_BLOQUE>'],
         'eof': []
     },
     '<BLOQUE>': {
-        'llave_de_inicio': ['<BLOQUE>','<_BLOQUE>'],
+        'llave_de_inicio': ['llave_de_inicio', '<BLOQUE>', '<_BLOQUE>', 'llave_de_cierre'],
         'identificador': ['identificador','punto_coma'],
         'bucle_while': ['bucle_while', 'parentesis_de_inicio', 'identificador', 'parentesis_de_cierre', '<BLOQUE>'],
         'bucle_do': ['bucle_do', '<BLOQUE>', 'bucle_while', 'parentesis_de_inicio', 'identificador', 'parentesis_de_cierre'],
